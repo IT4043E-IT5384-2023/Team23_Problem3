8cf10f99bdae555936909082683a46209b80a204
quangbinh113 <binhquang1103@gmail.com>
2023-11-13 02:17:39 +0700

    update

--- main.py
+++ main.py
@@ -6,16 +6,16 @@ logger = setup_logger(__name__)
 
 #Number of scroll down refreshing
 numIter = NUM_ITER
-logger.info(f"Finish loading number of iteration from .env file")
+logger.info(f"Finish loading number of iteration from config file")
 #Interval between 2 Iteration
 iterInterval = ITER_INTERVAL
-logger.info(f"Finish loading iteration interval from .env file")
+logger.info(f"Finish loading iteration interval from config file")
 
 data_dir = DATA_DIR
-logger.info(f"Finish loading data directory from .env file")
+logger.info(f"Finish loading data directory from config file")
 
 topic = TOPIC
-logger.info(f"Finish loading topics from .env file")
+logger.info(f"Finish loading topics from config file")
 
 
 if __name__ == "__main__":
